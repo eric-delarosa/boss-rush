db324bd88a6408dd459c83c8325e17757c244a5f
eric-delarosa <ericd.delarosa@gmail.com>
2021-06-11 18:19:36 -0400

    alerts for arrow key movement

--- src/input.js
+++ src/input.js
@@ -2,7 +2,23 @@ export default class InputHandler {
 
     constructor() {
         document.addEventListener('keydown', event => {
-            alert(event.code);
+            switch (event.code) {
+                case ArrowLeft:
+                    alert('move left');
+                    break;
+                case ArrowRight:
+                    alert('move right');
+                    break;
+                case ArrowUp:
+                    alert('move up');
+                    break;
+                case ArrowDown:
+                    alert('move down');
+                    break;
+                    
+                default:
+                    break;
+            }
         })
     }
 }
