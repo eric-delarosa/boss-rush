7a26c0c60add1c0a3b62463eddac21e75dff9313
eric-delarosa <ericd.delarosa@gmail.com>
2021-06-11 18:38:20 -0400

    testing with deprecated keyCode

--- src/input.js
+++ src/input.js
@@ -2,23 +2,24 @@ export default class InputHandler {
 
     constructor() {
         document.addEventListener('keydown', event => {
-            switch (event.code) {
-                case ArrowLeft:
-                    alert('move left');
-                    break;
-                case ArrowRight:
-                    alert('move right');
-                    break;
-                case ArrowUp:
-                    alert('move up');
-                    break;
-                case ArrowDown:
-                    alert('move down');
-                    break;
+            alert(event.keyCode);
+            // switch (event.keyCode) {
+            //     case "ArrowLeft":
+            //         alert('move left');
+            //         break;
+            //     case "ArrowRight":
+            //         alert('move right');
+            //         break;
+            //     case "ArrowUp":
+            //         alert('move up');
+            //         break;
+            //     case "ArrowDown":
+            //         alert('move down');
+            //         break;
                     
-                default:
-                    break;
-            }
+            //     default:
+            //         break;
+            // }
         })
     }
 }
